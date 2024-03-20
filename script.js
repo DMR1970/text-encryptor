25781195b5944c246dc2bac7ac7daac162e11e61
DMR1970 <denmejram@gmail.com>
2024-03-20 11:35:47 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const textArea = document.querySelector(".text-area");
+const message = document.querySelector(".message");
+
+let encryptions = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat']
+];
