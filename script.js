2e11e5a9c9621a079ca2c2f063bc01a567db0fa9
DMR1970 <denmejram@gmail.com>
2024-03-20 13:07:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const textArea = document.querySelector(".text-area");
 const message = document.querySelector(".message");
 
+function encryptionBttn(){
+    const encryptedText = encryption(textArea.value)
+}
+
 function encryption(stringEncrypted){
     let conversions = [
         ['e', 'enter'],
@@ -13,7 +17,7 @@ function encryption(stringEncrypted){
 
     for(let i = 0; i < conversions.length; i++){
         if(stringEncrypted.includes(conversions[i][0])){
-            stringEncrypted = stringEncrypted.replaceAll(conversions[i][0], conversions[i][1])
+            stringEncrypted = stringEncrypted.replaceAll(conversions[i][0], conversions[i][1]);
         }
 
     }
